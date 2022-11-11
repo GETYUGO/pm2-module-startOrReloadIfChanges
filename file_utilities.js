8b0b2a0cdb7b8fecf9732be2da423eea0368fa57
AlexisCra <53001247+AlexisCra@users.noreply.github.com>
2022-11-11 15:31:07 +0100

    V2 (#3)
    * feat: parse for requires and use it for generate checksum
    
    * feat: optimize require search and add action for refreshing checksums
    
    * feat: better retur message
    
    * feat: infinit loop protection
    
    * feat: update readme

--- file_utilities.js
+++ file_utilities.js
@@ -1,4 +1,30 @@
 const fs = require('fs');
+const uglify = require('uglify-js');
+
+const getFileName = (filePath) => {
+  const lastIndex = filePath.lastIndexOf('/');
+
+  return filePath.slice(lastIndex - filePath.length + 1);
+}
+
+const getFileBasePath = (filePath) => {
+  const lastIndex = filePath.lastIndexOf('/');
+
+  return filePath.slice(0, lastIndex + 1);
+}
+
+const toFilename = (requireEntry) => requireEntry.endsWith('.js') ? requireEntry : requireEntry + '.js';
+
+const parseRequires = (fileContent) => {
+  const re = /(?:require\('?"?)(.*?)(?:'?"?\))/gm;
+  const requirements = [];
+  let matches;
+
+  while ((matches = re.exec(fileContent)) !== null) {
+    requirements.push(matches[1]);
+  }
+  return requirements;
+}
 
 const fileExists = (filePath) => fs.existsSync(filePath);
 
@@ -11,6 +37,52 @@ const getFileContent = (filePath) => {
 
 const getFileJson = (filePath) => JSON.parse(getFileContent(filePath));
 
+const getModulePackageVersion = (nodeModulesPath, moduleName) => {
+  const modulePath = nodeModulesPath + moduleName + '/';
+  const packageContent = getFileContent(modulePath + 'package.json');
+
+  return JSON.parse(packageContent).version;
+}
+
+const getFileAndRequirements = (filePath, nodeModulesPath, depth = 0) => {
+  const fileContent = getFileContent(filePath);
+
+  if (depth > 10) { // prevent infinite loop
+    console.error('TOO MUCH DEPTH:', filePath);
+    return {
+      [getFileName(filePath)]: uglify.minify(fileContent).code,
+    }
+  }
+  const requirements = parseRequires(fileContent);
+
+  return requirements.reduce(
+    (prev, cur) => {
+      if (!cur.startsWith('.')) {
+        try {
+          const packageVersion = getModulePackageVersion(nodeModulesPath, cur);
+
+          return {
+            ...prev,
+            [cur]: packageVersion,
+          }
+        } catch (e) {
+          return prev;
+        }
+      }
+      const curPath = getFileBasePath(filePath) + toFilename(cur);
+
+      return {
+        ...prev,
+        ...getFileAndRequirements(curPath, nodeModulesPath),
+      }
+
+    },
+    {
+      [getFileName(filePath)]: uglify.minify(fileContent).code,
+    },
+  );
+}
+
 const putFileContent = (filePath, content) => {
   fs.writeFileSync(filePath, content);
 }
@@ -19,5 +91,6 @@ module.exports = {
   fileExists,
   getFileContent,
   getFileJson,
+  getFileAndRequirements,
   putFileContent,
 };
--- md5.js
+++ md5.js
@@ -1,9 +1,14 @@
 const md5 = require('md5');
-const { getFileContent } = require('./file_utilities');
+const { getFileAndRequirements } = require('./file_utilities');
+
+const toMinimalJSON = (data) => JSON.stringify(data, undefined, 0);
 
 const getCurrentMd5 = (basePath, apps) => apps.reduce((prev, app) => ({
   ...prev,
-  [app.name]: md5(getFileContent(`${basePath}/${app.script}`)),
+  [app.name]: md5(toMinimalJSON(getFileAndRequirements(
+    `${basePath}/${app.script}`,
+    `${basePath}/node_modules/`,
+  ))),
 }), {});
 
 module.exports = {
