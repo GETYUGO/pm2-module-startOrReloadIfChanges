f29e5ae654b40927abce5b52766996c27423dad6
Alexis Craye <alexis.craye@rideyego.com>
2022-11-11 12:40:50 +0100

    feat: parse for requires and use it for generate checksum

--- file_utilities.js
+++ file_utilities.js
@@ -1,4 +1,29 @@
 const fs = require('fs');
+const uglify = require('uglify-js');
+
+const getFileName = (filePath) => {
+  const lastIndex = filePath.lastIndexOf('/');
+
+  return filePath.slice(lastIndex - filePath.length + 1);
+}
+
+const getFileBasePath = (filePath) => {
+  const lastIndex = filePath.lastIndexOf('/');
+
+  return filePath.slice(0, lastIndex + 1);
+}
+
+const toFilename = (requireEntry) => requireEntry.endsWith('.js') ? requireEntry : requireEntry + '.js';
+
+const parseRequires = (fileContent) => fileContent.split('\n').reduce((prev, line) => {
+  const re = /(?:require\('?"?)(.*?)(?:'?"?\))/;
+  const matches = re.exec(line);
+
+  if (matches) {
+    return [...prev, matches[1]];
+  }
+  return prev;
+}, []);
 
 const fileExists = (filePath) => fs.existsSync(filePath);
 
@@ -11,6 +36,45 @@ const getFileContent = (filePath) => {
 
 const getFileJson = (filePath) => JSON.parse(getFileContent(filePath));
 
+const getModulePackageVersion = (nodeModulesPath, moduleName) => {
+  const modulePath = nodeModulesPath + moduleName + '/';
+  const packageContent = getFileContent(modulePath + 'package.json');
+
+  return JSON.parse(packageContent).version;
+}
+
+const getFileAndRequirements = (filePath, nodeModulesPath) => {
+  const fileContent = getFileContent(filePath);
+  const requirements = parseRequires(fileContent);
+
+  return requirements.reduce(
+    (prev, cur) => {
+      if (!cur.startsWith('.')) {
+        try {
+          const packageVersion = getModulePackageVersion(nodeModulesPath, cur);
+
+          return {
+            ...prev,
+            [cur]: packageVersion,
+          }
+        } catch (e) {
+          return prev;
+        }
+      }
+      const curPath = getFileBasePath(filePath) + toFilename(cur);
+
+      return {
+        ...prev,
+        ...getFileAndRequirements(curPath, nodeModulesPath),
+      }
+
+    },
+    {
+      [getFileName(filePath)]: uglify.minify(fileContent).code,
+    },
+  );
+}
+
 const putFileContent = (filePath, content) => {
   fs.writeFileSync(filePath, content);
 }
@@ -19,5 +83,6 @@ module.exports = {
   fileExists,
   getFileContent,
   getFileJson,
+  getFileAndRequirements,
   putFileContent,
 };
